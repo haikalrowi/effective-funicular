2b2b6e8d32908885dee5d7f57ff66b6340f97991
Haikal Rowi <haikalbintang27@gmail.com>
2022-11-07 12:51:35 +0700

    optimized

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ http.createServer((req, res) => {
   if (fs.existsSync(req.url)) {
     if (fs.statSync(req.url).isDirectory()) {
       res.end(
-        fs.readFileSync(path.join(req.url, 'index.html'))
+        fs.readFile(path.join(req.url, 'index.html'), (err, data) => {
+          if (err) {
+            res.end(http.STATUS_CODES[404])
+          } else if (data) {
+            res.end(data)
+          }
+        })
       )
-    } else {
+    } else if (fs.statSync(req.url).isFile()) {
       res.end(
         fs.readFileSync(req.url)
       )
