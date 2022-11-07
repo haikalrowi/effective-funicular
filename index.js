ceb6a2a2ca860217e76540a6150331fd05401a1d
Haikal Rowi <haikalbintang27@gmail.com>
2022-11-07 13:16:07 +0700

    ~

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ http.createServer((req, res) => {
   req.url = path.join(__dirname, 'root', req.url)
   if (fs.existsSync(req.url)) {
     if (fs.statSync(req.url).isDirectory()) {
-      res.end(
-        fs.readFile(path.join(req.url, 'index.html'), (err, data) => {
-          if (err) {
-            res.end(http.STATUS_CODES[404])
-          } else if (data) {
-            res.end(data)
-          }
-        })
-      )
+      fs.readFile(path.join(req.url, 'index.html'), (err, data) => {
+        if (err) {
+          res.end(http.STATUS_CODES[404])
+        } else if (data) {
+          res.end(data)
+        }
+      })
     } else if (fs.statSync(req.url).isFile()) {
       res.end(
         fs.readFileSync(req.url)
